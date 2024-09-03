ba5bb839e7e9e2ad051abf1151e94a4fb0bb4d52
Samuel Vary <samuelvary@samuels-air.mynetworksettings.com>
2024-09-03 14:34:36 -0400

    change proxy-server index.js file to use dynamic import

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const app = express();
 app.use(cors());
